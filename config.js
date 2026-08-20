d28ebe51e1f3a44220e0104296c5db7c4c4484a8
bangwadcharityrun1-png <bangwadcharityrun1@gmail.com>
2026-08-20 19:31:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://YOUR_PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR_ANON_KEY',
+  SUPABASE_URL: 'https://obvqofyobnjtqmhhgtgr.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9idnFvZnlvYm5qdHFtaGhndGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjM5NDMsImV4cCI6MjEwMjc5OTk0M30.53G2mKzJbLWUAjY-VfncZCrX4lk5TWjwOuK4bCpWJ0A',
   EVENT_SLUG: 'top100-charity-run'
 };
